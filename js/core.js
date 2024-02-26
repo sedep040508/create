00b20d05ab5604985d1bcbad1e8ca4c4cf338134
sedep040508 <elikbaevasedeppka@gmail.com>
2024-02-26 14:24:33 +0600

    added prigress

--- js/core.js
+++ js/core.js
@@ -139,25 +139,27 @@ for (i = 0; i < acc.length; i++) {
 // >>> > 006 db1d2b8ae7b26e5814e0b6f7ac08e8ab80066
 
 
-(function(){
+(function () {
 
   let counter = document.querySelectorAll('.counter');
   let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
-  window.addEventListener('scroll', function(){  
-    if( limit == counter.length ){ return; }
-    
-    for(let i = 0; i < counter.length; i++){
+  window.addEventListener('scroll', function () {
+    if (limit == counter.length) {
+      return;
+    }
+
+    for (let i = 0; i < counter.length; i++) {
       let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
       let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
-      if( pos < win && counter[i].dataset.stop === "0" ){
+      if (pos < win && counter[i].dataset.stop === "0") {
         counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
         let x = 0;
         limit++; // Счетчик будет запущен, увеличиваем переменную на 1
-        let int = setInterval(function(){
+        let int = setInterval(function () {
           // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
-          x = x + Math.ceil( counter[i].dataset.to / 20 ); 
+          x = x + Math.ceil(counter[i].dataset.to / 20);
           counter[i].innerText = x;
-          if( x > counter[i].dataset.to ){
+          if (x > counter[i].dataset.to) {
             //Как только досчитали - стираем интервал.
             counter[i].innerText = counter[i].dataset.to;
             clearInterval(int);
@@ -166,39 +168,39 @@ for (i = 0; i < acc.length; i++) {
       }
     }
   });
-  
-  })();
+
+})();
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
-    'use strict'
+  'use strict'
 
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
 
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
+        form.classList.add('was-validated')
+      }, false)
+    })
 })()
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const videoButton = document.querySelector('.pulse');
   const videoContainer = document.getElementById('videoContainer');
 
-  videoButton.addEventListener('click', function() {
+  videoButton.addEventListener('click', function () {
     videoContainer.classList.remove('hidden');
     videoButton.classList.add('hidden');
-    
+
   });
 });
 
@@ -283,3 +285,180 @@ function closeOnClick() {
   hamb.classList.remove("active");
   body.classList.remove("noscroll");
 }
+
+(() => {
+  'use strict'
+  const forms = document.querySelectorAll('.needs-validation')
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      // panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+(function (window) {
+  'use strict';
+
+  function classReg(className) {
+    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+  }
+  var hasClass, addClass, removeClass;
+
+  if ('classList' in document.documentElement) {
+    hasClass = function (elem, c) {
+      return elem.classList.contains(c);
+    };
+    addClass = function (elem, c) {
+      elem.classList.add(c);
+    };
+    removeClass = function (elem, c) {
+      elem.classList.remove(c);
+    };
+  } else {
+    hasClass = function (elem, c) {
+      return classReg(c).test(elem.className);
+    };
+    addClass = function (elem, c) {
+      if (!hasClass(elem, c)) {
+        elem.className = elem.className + ' ' + c;
+      }
+    };
+    removeClass = function (elem, c) {
+      elem.className = elem.className.replace(classReg(c), ' ');
+    };
+  }
+
+  function toggleClass(elem, c) {
+    var fn = hasClass(elem, c) ? removeClass : addClass;
+    fn(elem, c);
+  }
+  var classie = {
+    hasClass: hasClass,
+    addClass: addClass,
+    removeClass: removeClass,
+    toggleClass: toggleClass,
+    has: hasClass,
+    add: addClass,
+    remove: removeClass,
+    toggle: toggleClass
+  };
+  if (typeof define === 'function' && define.amd) {
+    define(classie);
+  } else {
+    window.classie = classie;
+  }
+})(window);
+var $ = function (selector) {
+  return document.querySelector(selector);
+}
+var accordion = $('.accordion');
+accordion.addEventListener("click", function (e) {
+  e.stopPropagation();
+  e.preventDefault();
+  if (e.target && e.target.nodeName == "A") {
+    var classes = e.target.className.split(" ");
+    if (classes) {
+      for (var x = 0; x < classes.length; x++) {
+        if (classes[x] == "accordionTitle") {
+          var title = e.target;
+          var content = e.target.parentNode.nextElementSibling;
+          classie.toggle(title, 'accordionTitleActive');
+          if (classie.has(content, 'accordionItemCollapsed')) {
+            if (classie.has(content, 'animateOut')) {
+              classie.remove(content, 'animateOut');
+            }
+            classie.add(content, 'animateIn');
+          } else {
+            classie.remove(content, 'animateIn');
+            classie.add(content, 'animateOut');
+          }
+          classie.toggle(content, 'accordionItemCollapsed');
+        }
+      }
+    }
+  }
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+(function () {
+
+  let counter = document.querySelectorAll('.counter');
+  let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
+  window.addEventListener('scroll', function () {
+    if (limit == counter.length) {
+      return;
+    }
+
+    for (let i = 0; i < counter.length; i++) {
+      let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
+      let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
+      if (pos < win && counter[i].dataset.stop === "0") {
+        counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
+        let x = 0;
+        limit++; // Счетчик будет запущен, увеличиваем переменную на 1
+        let int = setInterval(function () {
+          // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
+          x = x + Math.ceil(counter[i].dataset.to / 50);
+          counter[i].innerText = x;
+          if (x > counter[i].dataset.to) {
+            //Как только досчитали - стираем интервал.
+            counter[i].innerText = counter[i].dataset.to;
+            clearInterval(int);
+          }
+        }, 30);
+      }
+    }
+  })
+})();
+
+function changeColor() {
+  const card = document.querySelector('.flexible_card');
+  card.classList.toggle('clicked');
+}
+
+function changeColor2() {
+  const card = document.querySelector('.flexible_card_2');
+  card.classList.toggle('clicked');
+}
+
+function changeColor3() {
+  const card = document.querySelector('.flexible_card_3');
+  card.classList.toggle('clicked');
+}
+
+let switch_button1 = document.getElementById('flexible_switch-button');
+switch_button1.addEventListener('click', function () {
+  this.classList.toggle('switch-active1')
+})
